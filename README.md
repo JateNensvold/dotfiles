2edbf538afb6a391602fc454826df80001398d8a
Nate Jensvold <jensvoldnate@gmail.com>
2023-05-26 07:06:46 +0000

    Added base implementation for gathering some dotfile information?

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Dotfiles
-This repo contains a 
+This Repo containing all the dotfiles used to support my development workflows.
+Additionally there is a simple rhai(rust script) program that can be used for backing up and
+managing env/global config files.
+
+# Usage
+
+
--- dotfile-rs/src/main.rs
+++ dotfile-rs/src/main.rs
@@ -1,3 +1,37 @@
+use directories::BaseDirs;
+use regex::Regex;
+use std::env::{var, VarError};
+use std::path::{Path, PathBuf};
+
 fn main() {
     println!("Hello, world!");
+
+    let dotfile = DotFile::default();
+}
+
+struct EnvironmentVariableNames {}
+
+impl EnvironmentVariableNames {
+    const SHELL: &'static str = "SHELL";
+}
+
+struct DotFile {
+    ignore: Regex,
+    shell: String,
+    home: PathBuf,
+    root: PathBuf,
+}
+
+impl Default for DotFile {
+    fn default() -> Self {
+        let base_dir = BaseDirs::new()
+            .expect("Unable to determine location of common system directories such as $HOME");
+
+        Self {
+            ignore: Regex::new(r"").unwrap(),
+            shell: var(EnvironmentVariableNames::SHELL).unwrap_or(String::from("Unknown")),
+            home: base_dir.home_dir().to_path_buf(),
+            root: Path::new("").to_path_buf(),
+        }
+    }
 }
